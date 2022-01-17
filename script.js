47dbd52d02e19c4a79c19fe86e98d3e71b3598de
JeanOviedo <85713073+JeanOviedo@users.noreply.github.com>
2022-01-17 12:01:22 -0500

    Finalizado el proyecto

--- script.js
+++ script.js
@@ -20,13 +20,17 @@ No se permite acentuación de palabras
 
 window.addEventListener("load", cargaPagina);
 function cargaPagina() {
-  var btn = document
+  let btn = document
     .getElementById("btn-encriptar")
     .addEventListener("click", cambiaValores);
 
-  var btn2 = document
+  let btn2 = document
     .getElementById("btn-desencriptar")
     .addEventListener("click", cambiaValoresDes);
+
+  let btn3 = document
+    .getElementById("btn-copy")
+    .addEventListener("click", Copiado);
 }
 
 const transactionForm = document.getElementById("transactionForm");
@@ -36,34 +40,62 @@ transactionForm.addEventListener("submit", function (event) {
 
 function cambiaValores() {
   let textoencriptar = document.getElementById("input-texto").value;
+  if (!textoencriptar) {
+    div1 = document.getElementById("rellene");
+    div1.style.display = "";
+  } else {
+    let encriptando = textoencriptar.replace(/e/g, "enter");
+    encriptando = encriptando.replace(/i/g, "imes");
+    console.log("imes", encriptando);
+    encriptando = encriptando.replace(/a/g, "ai");
+    console.log("ai", encriptando);
+    encriptando = encriptando.replace(/o/g, "ober");
+    console.log("o", encriptando);
+    encriptando = encriptando.replace(/u/g, "ufat");
+    console.log("u", encriptando);
 
-  let encriptando = textoencriptar.replace(/e/g, "enter");
-  encriptando = encriptando.replace(/i/g, "imes");
-  console.log("imes", encriptando);
-  encriptando = encriptando.replace(/a/g, "ai");
-  console.log("ai", encriptando);
-  encriptando = encriptando.replace(/o/g, "ober");
-  console.log("o", encriptando);
-  encriptando = encriptando.replace(/u/g, "ufat");
-  console.log("u", encriptando);
-
-  var inputNombre = document.getElementById("msg");
-  inputNombre.value = encriptando;
+    let inputNombre = document.getElementById("msg");
+    inputNombre.value = encriptando;
+    div = document.getElementById("copi");
+    div.style.display = "none";
+    div3 = document.getElementById("rellene");
+    div3.style.display = "none";
+  }
 }
 
 function cambiaValoresDes() {
   let textodesencriptar = document.getElementById("input-texto").value;
 
-  let encriptando = textodesencriptar.replace(/enter/g, "e");
-  encriptando = encriptando.replace(/imes/g, "i");
-  console.log("imes", encriptando);
-  encriptando = encriptando.replace(/ai/g, "a");
-  console.log("ai", encriptando);
-  encriptando = encriptando.replace(/ober/g, "o");
-  console.log("o", encriptando);
-  encriptando = encriptando.replace(/ufat/g, "u");
-  console.log("u", encriptando);
+  if (!textodesencriptar) {
+    div1 = document.getElementById("rellene");
+    div1.style.display = "";
+  } else {
+    let encriptando = textodesencriptar.replace(/enter/g, "e");
+    encriptando = encriptando.replace(/imes/g, "i");
+    console.log("imes", encriptando);
+    encriptando = encriptando.replace(/ai/g, "a");
+    console.log("ai", encriptando);
+    encriptando = encriptando.replace(/ober/g, "o");
+    console.log("o", encriptando);
+    encriptando = encriptando.replace(/ufat/g, "u");
+    console.log("u", encriptando);
 
-  var inputNombre = document.getElementById("msg");
-  inputNombre.value = encriptando;
+    let inputNombre = document.getElementById("msg");
+    inputNombre.value = encriptando;
+    div = document.getElementById("copi");
+    div.style.display = "none";
+    div3 = document.getElementById("rellene");
+    div3.style.display = "none";
+  }
+}
+
+function Copiado() {
+  div = document.getElementById("copi");
+  div.style.display = "";
+}
+
+function copyToClipBoard() {
+  let content = document.getElementById("msg");
+  content.select();
+  document.execCommand("copy");
 }
