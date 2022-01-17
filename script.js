9644c002af06f236ae522617830255a3f20e6acc
JeanOviedo <85713073+JeanOviedo@users.noreply.github.com>
2022-01-17 11:34:19 -0500

    Agregado encriptador y desencriptador

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 /* Reglas de encriptación: 
 "e" es convertido para "enter" 
 "i" es convertido para "imes"
@@ -18,3 +17,53 @@ No se permite acentuación de palabras
 Solo letras minusculas
 No se permite acentuación de palabras   
 */
+
+window.addEventListener("load", cargaPagina);
+function cargaPagina() {
+  var btn = document
+    .getElementById("btn-encriptar")
+    .addEventListener("click", cambiaValores);
+
+  var btn2 = document
+    .getElementById("btn-desencriptar")
+    .addEventListener("click", cambiaValoresDes);
+}
+
+const transactionForm = document.getElementById("transactionForm");
+transactionForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+});
+
+function cambiaValores() {
+  let textoencriptar = document.getElementById("input-texto").value;
+
+  let encriptando = textoencriptar.replace(/e/g, "enter");
+  encriptando = encriptando.replace(/i/g, "imes");
+  console.log("imes", encriptando);
+  encriptando = encriptando.replace(/a/g, "ai");
+  console.log("ai", encriptando);
+  encriptando = encriptando.replace(/o/g, "ober");
+  console.log("o", encriptando);
+  encriptando = encriptando.replace(/u/g, "ufat");
+  console.log("u", encriptando);
+
+  var inputNombre = document.getElementById("msg");
+  inputNombre.value = encriptando;
+}
+
+function cambiaValoresDes() {
+  let textodesencriptar = document.getElementById("input-texto").value;
+
+  let encriptando = textodesencriptar.replace(/enter/g, "e");
+  encriptando = encriptando.replace(/imes/g, "i");
+  console.log("imes", encriptando);
+  encriptando = encriptando.replace(/ai/g, "a");
+  console.log("ai", encriptando);
+  encriptando = encriptando.replace(/ober/g, "o");
+  console.log("o", encriptando);
+  encriptando = encriptando.replace(/ufat/g, "u");
+  console.log("u", encriptando);
+
+  var inputNombre = document.getElementById("msg");
+  inputNombre.value = encriptando;
+}
